8515629d262163c40b204975c19d80b2da5a3fcf
Nikita <nikita456nikita@yandex.ru>
2024-08-23 15:13:09 +0300

    feat: add server and configure prettier and eslint

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'I know who you are');
+});
